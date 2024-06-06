11f19a25f84d5b2185c2551d9976252ce369b34b
Mavlon4658 <mavlonbe4658>
2024-06-06 14:49:04 +0500

    tour page is ready

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,14 @@ import SignInPage from "@/components/pages/SignInPage.vue";
 import SignUpPage from "@/components/pages/SignUpPage.vue";
 import UserPage from "@/components/pages/UserPage.vue";
 import HomePage from "@/components/pages/HomePage.vue"
+import TourPage from '@/components/pages/TourPage.vue'
 
 const routes = [
 	{path: "/", component: HomePage},
 	{path: "/sign-in", component: SignInPage},
 	{path: "/sign-up", component: SignUpPage},
 	{path: "/user/:email", name: "user", component: UserPage},
+	{path: "/tour/:id", name: 'tour', component: TourPage}
 ];
 
 const router = createRouter({
